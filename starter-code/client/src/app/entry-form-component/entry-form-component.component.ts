fc7dc24697a4db8c05d18af7f1e247f0104e4b90
popelini <ccastillon92@gmail.com>
2017-11-30 22:29:55 +0100

    Auto update and from cleanup added

--- starter-code/client/src/app/entry-form-component/entry-form-component.component.ts
+++ starter-code/client/src/app/entry-form-component/entry-form-component.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {JournalService} from '../../services/journal.service';
 import {ActivatedRoute, Router} from '@angular/router';
-
+import { EntryListComponent } from '../entry-list/entry-list.component';
 
 @Component({
   selector: 'app-entry-form-component',
@@ -13,14 +13,16 @@ export class EntryFormComponentComponent implements OnInit {
   constructor(
     public journalService: JournalService,
     public route: ActivatedRoute,
-    public router: Router
+    public router: Router,
+    public entryList : EntryListComponent,
   ) {
 
   }
   ngOnInit() { }
   addEntry() {
     this.journalService.createNewEntry(this.newEntry).subscribe(() => {
-      this.router.navigate(['/']);
+      this.entryList.getAllList();
     })
+    this.newEntry = {};
   }
 }
